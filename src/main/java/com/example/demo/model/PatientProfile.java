66f21f6f8225f537a9cb18ba1a03259e8b574170
bala-amypo <bala@amypo.in>
2025-12-18 08:09:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/PatientProfile.java
@@ -0,0 +1,11 @@
+public class PaitentProfile {
+    private Long id;
+    private String patientid;
+    private String fullname;
+    private int age;
+    private String email;
+    private String surgeryType;
+    private boolean active;
+    private LocalDate createdAt;
+
+}
