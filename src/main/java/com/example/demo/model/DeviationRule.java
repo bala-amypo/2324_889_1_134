1c10ca2c1347e7336ec68756eb39f2c1fad3984b
bala-amypo <bala@amypo.in>
2025-12-18 13:02:30 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviationRule.java
+++ src/main/java/com/example/demo/model/DeviationRule.java
@@ -1,4 +1,5 @@
 package com.example.demo.model;
+import jakarta.persistence.Id;
 
 public class DeviationRule{
     @Id
--- src/main/java/com/example/demo/model/RecoveryCurveProfile.java
+++ src/main/java/com/example/demo/model/RecoveryCurveProfile.java
@@ -1,3 +1,5 @@
+package com.example.demo.model;
+import jakarta.persistence.Id;
 
 public class RecoveryCurveProfile{
     @Id
@@ -7,6 +9,80 @@ public class RecoveryCurveProfile{
     private int expectedPainLevel;
     private int expectedMobilityLevel;
     private int expectedFatigueLevel;
+
+public RecoveryCurveProfile(Long id, String surgeryType, int dayNumber, int expectedPainLevel,
+            int expectedMobilityLevel, int expectedFatigueLevel) {
+        this.id = id;
+        this.surgeryType = surgeryType;
+        this.dayNumber = dayNumber;
+        this.expectedPainLevel = expectedPainLevel;
+        this.expectedMobilityLevel = expectedMobilityLevel;
+        this.expectedFatigueLevel = expectedFatigueLevel;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+
+
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
+
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+
+    public void setDayNumber(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+
+    public int getExpectedPainLevel() {
+        return expectedPainLevel;
+    }
+
+
+    public void setExpectedPainLevel(int expectedPainLevel) {
+        this.expectedPainLevel = expectedPainLevel;
+    }
+
+
+    public int getExpectedMobilityLevel() {
+        return expectedMobilityLevel;
+    }
+
+
+    public void setExpectedMobilityLevel(int expectedMobilityLevel) {
+        this.expectedMobilityLevel = expectedMobilityLevel;
+    }
+
+
+    public int getExpectedFatigueLevel() {
+        return expectedFatigueLevel;
+    }
+
+
+    public void setExpectedFatigueLevel(int expectedFatigueLevel) {
+        this.expectedFatigueLevel = expectedFatigueLevel;
+    }
+
+
+    public RecoveryCurveProfile() {
+    }
 
 
 }
