344e15b38016615200e85aa60b2efb8dc8cbf723
bala-amypo <bala@amypo.in>
2025-12-18 08:54:25 +0000

    User commit

--- src/main/java/com/example/demo/model/RecoveryCurveProfile.java
+++ src/main/java/com/example/demo/model/RecoveryCurveProfile.java
@@ -1,4 +1,11 @@
 
 public class RecoveryCurveProfile{
+    @Id
+    private Long id;
+    private String surgeryType;
+    private int dayNumber;
+    private int ;
+    private int expectedFatigueLevel;
+
 
 }
