01c053093c1ae92cd81cf1dcd24aaf0b1a115b17
bala-amypo <bala@amypo.in>
2025-12-18 18:40:47 +0000

    User commit

--- src/main/java/com/example/demo/service/RecoveryCurveService.java
+++ src/main/java/com/example/demo/service/RecoveryCurveService.java
@@ -1,3 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Recovery;
+import com.example.demo.model.RecoveryCurve;
+
+public interface RecoveryCurveService{
+`
+    RecoveryCurve createCurveEntry(RecoveryCurveProfile entry);
+
+    RecoveryCurve getCurveForSurgery(String surgeryType);
+
+    List<RecoveryCurve> getAllCurves();
+}
