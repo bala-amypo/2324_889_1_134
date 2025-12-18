5eaa6c9757cff5b9db91b124f4f7b9d1954847c2
bala-amypo <bala@amypo.in>
2025-12-18 13:08:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ClinicalAlert.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+import jakarta.persistence.Id;
+
+public class ClinicalAlert {
+    @Id
+    private Long id;
+    private PaitentProfile paitent;
+    private Local
+}
