73405c071f71feb1189790e97e8cd933defff0ee
bala-amypo <bala@amypo.in>
2025-12-18 09:49:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -1,1 +1,6 @@
-public inteface AppUserRepository extends JpaRepository<AppUser
+package.com.example.demo.repository;
+import com.example.demo.model.AppUser;
+
+public inteface AppUserRepository extends JpaRepository<AppUser,Long>{
+
+}
