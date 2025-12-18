bad7127bb943dd788e1721c7e35b456d3e99ee50
bala-amypo <bala@amypo.in>
2025-12-18 15:49:46 +0000

    User commit

--- src/main/java/com/example/demo/model/ClinicalAlert.java
+++ src/main/java/com/example/demo/model/ClinicalAlert.java
@@ -8,4 +8,55 @@ public class ClinicalAlert {
     private LocalDate alterDate;
     private Severity severity; 
     private boolean resolved;
+    
+    public ClinicalAlert(Long id, PaitentProfile paitent, LocalDate alterDate, Severity severity, boolean resolved) {
+        this.id = id;
+        this.paitent = paitent;
+        this.alterDate = alterDate;
+        this.severity = severity;
+        this.resolved = resolved;
+    }
+
+    public ClinicalAlert() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PaitentProfile getPaitent() {
+        return paitent;
+    }
+
+    public void setPaitent(PaitentProfile paitent) {
+        this.paitent = paitent;
+    }
+
+    public LocalDate getAlterDate() {
+        return alterDate;
+    }
+
+    public void setAlterDate(LocalDate alterDate) {
+        this.alterDate = alterDate;
+    }
+
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(Severity severity) {
+        this.severity = severity;
+    }
+
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        this.resolved = resolved;
+    }
 }
