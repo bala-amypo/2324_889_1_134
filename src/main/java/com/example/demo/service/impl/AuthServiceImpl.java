4f75a86cb228b02216a953bb3a20ab3ba15feb1f
bala-amypo <bala@amypo.in>
2025-12-19 05:01:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,27 +1,47 @@
 package com.example.demo.service;
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.model.AppUser;
 import com.example.demo.repository.AppUserRepository;
+import org.springframework.stereotype.Service;
 
 @Service
-public interface ClinicalAlertServiceImpl implements ClinicalAlert{
+public class AuthServiceImpl implements AuthService {
 
-private final ClinicalAlertRepository repository;
+    private final AppUserRepository repository;
 
-public AuthServiceImpl(AppUserRepository repository){
-    this.repository=repository;
-}
+    public AuthServiceImpl(AppUserRepository repository) {
+        this.repository = repository;
+    }
 
-@Override
-public AppUser register(RegisterRequest request){
-    return repository.save(alert);
-}
-@Override
-public List<ClinicalAlert> getAlertByPatient(Long patientid){
-    return repository.findById(patientid);
-}
-@Override
-public ClinicalAlert getLogById(Long id){
-    return repository.findById(Long id);
-}
+    @Override
+    public AuthResponse register(RegisterRequest request) {
 
-} 
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        AppUser user = new AppUser();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword()); 
+        user.setRole("USER");
+
+        repository.save(user);
+
+        return new AuthResponse("User registered successfully");
+    }
+    @Override
+    public AuthResponse login(AuthRequest request) {
+
+        AppUser user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return new AuthResponse("Login successful");
+    }
+}
