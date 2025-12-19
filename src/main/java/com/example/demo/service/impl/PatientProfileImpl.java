adbd1bb687d999b36382ffb2306cb5d91339756f
bala-amypo <bala@amypo.in>
2025-12-19 01:37:17 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PatientProfileImpl.java
+++ src/main/java/com/example/demo/service/impl/PatientProfileImpl.java
@@ -5,5 +5,15 @@ import com.example.demo.repository.PatientProfileRepository;
 @Service
 public interface PatientProfileImpl implements PaitentProfile{
 
-private final PatientProfileRepository
+private final PatientProfileRepository repository;
+
+public PatientProfileImpl(PatientProfileRepository repository){
+    this.repository=repository;
+}
+
+@Override
+public PatientProfile createPatient(Patient)
+
+
+
 } 
