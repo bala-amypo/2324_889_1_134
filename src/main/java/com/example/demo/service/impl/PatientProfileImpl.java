ba13dee1dd4c7dcd9402ab66364729e166081841
bala-amypo <bala@amypo.in>
2025-12-19 01:34:17 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PatientProfileImpl.java
+++ src/main/java/com/example/demo/service/impl/PatientProfileImpl.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 import com.example.demo.model.PaitentProfile;
 import com.example.demo.repository.PatientProfileRepository;
 
+@Service
 public interface PatientProfileImpl implements PaitentProfile{
-    
+
+private final PatientProfileRepository
 } 
